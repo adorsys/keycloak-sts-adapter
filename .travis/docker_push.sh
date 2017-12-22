86744748d8a97f05c1fefa38566f2a919c09b8b4
Boris Skert <boris.skert@adorsys.de>
2017-12-22 10:42:16 +0100

    initial travis-ci config to build and push docker image

---
+++ .travis/docker_push.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
+
+# ----- build and push docker image -----
+
+IMAGE_NAME="${DOCKER_IMAGE_NAME}:${TRAVIS_TAG}"
+LATEST_IMAGE_NAME="${DOCKER_IMAGE_NAME}:latest"
+
+docker build -t $IMAGE_NAME ./
+docker tag $IMAGE_NAME $LATEST_IMAGE_NAME
+
+docker push $IMAGE_NAME
+docker push $LATEST_IMAGE_NAME
